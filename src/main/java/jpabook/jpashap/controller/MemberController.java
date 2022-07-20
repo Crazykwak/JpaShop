bd4a42b5693566a6847200fcdecf35c9b6d32140
Crazykwak <kwj1830@naver.com>
2022-07-20 14:50:23 +0900

    Member form, templates

---
+++ src/main/java/jpabook/jpashap/controller/MemberController.java
@@ -0,0 +1,52 @@
+package jpabook.jpashap.controller;
+
+import jpabook.jpashap.entity.Address;
+import jpabook.jpashap.entity.Member;
+import jpabook.jpashap.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid MemberForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member(form.getName(), address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
